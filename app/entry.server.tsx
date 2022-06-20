3525cdf44bbc108d73b6024a73ed30a3b5957b1c
Anton Johansson <anton@department.se>
2022-06-20 08:12:39 +0200

    https

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,5 +1,5 @@
 import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
+import { redirect, RemixServer } from "remix";
 import type { EntryContext } from "remix";
 
 export default function handleRequest(
@@ -11,6 +11,16 @@ export default function handleRequest(
   let markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
+
+  const url = new URL(request.url);
+  if (url.hostname !== "localhost" && !url.protocol.startsWith("https")) {
+    return redirect(url.toString(), {
+      status: 301,
+      headers: {
+        "X-Forwarded-Proto": "https",
+      },
+    });
+  }
 
   responseHeaders.set("Content-Type", "text/html");
 
